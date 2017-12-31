4598385fb673162df937b3290ccaae7c7523e1e5
pokio <32353873+pokiiio@users.noreply.github.com>
2017-12-31 13:29:33 +0900

    Now you can add value1-3 to IFTTT triggers.

--- ESP8266IFTTT.cpp
+++ ESP8266IFTTT.cpp
@@ -1,10 +1,16 @@
 #include <ESP8266HTTPClient.h>
 #include "ESP8266IFTTT.h"
 
-void ESP8266IFTTT::trigger(String iftttEventName, String iftttKey) {
+void ESP8266IFTTT::trigger(String iftttEventName, String iftttKey)
+{
+  trigger(iftttEventName, iftttKey, "", "", "");
+}
+
+void ESP8266IFTTT::trigger(String iftttEventName, String iftttKey, String value1, String value2, String value3)
+{
   HTTPClient http;
-  String json = "{}";
-  http.begin("http://maker.ifttt.com/trigger/" + iftttEventName  + "/with/key/" + iftttKey);
+  String json = "{\"value1\":\"" + value1 + "\", \"value2\":\"" + value2 + "\", \"value3\":\"" + value3 + "\"}";
+  http.begin("http://maker.ifttt.com/trigger/" + iftttEventName + "/with/key/" + iftttKey);
   http.addHeader("Content-Type", "application/JSON");
   http.addHeader("Content-Length", String(json.length()));
   int httpCode = http.POST(json);
--- ESP8266IFTTT.h
+++ ESP8266IFTTT.h
@@ -7,9 +7,11 @@
 #include "WProgram.h"
 #endif
 
-class ESP8266IFTTT {
-    public:
-        void trigger(String iftttEventName, String iftttKey);
+class ESP8266IFTTT
+{
+  public:
+    void trigger(String iftttEventName, String iftttKey);
+    void trigger(String iftttEventName, String iftttKey, String value1, String value2, String value3);
 };
 
 extern ESP8266IFTTT IFTTT;
